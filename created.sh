a04acbeaf0fe6cadefa8c2fd468a5324c195fc98
nondualit <anibal@nondualit.nl>
2016-06-15 13:56:40 +0200

    Create created.sh

---
+++ created.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# create databases script by Anibal v2
+# without asking mysql passwd, passwd is stored in my.cnf
+echo -n "Enter dbname: "
+read var1
+echo -n "Enter user: "
+read var2
+echo -n "Enter passwd: "
+read var3
+
+
+echo ""         # force a carriage return to be output
+
+mysql -u root -e "create database $var1";
+mysql -u root -e "grant usage on *.* to $var2@localhost identified by '$var3'";
+mysql -u root -e "grant all privileges on $var1.* to $var2@localhost" ;
